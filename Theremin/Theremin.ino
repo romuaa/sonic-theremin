f63e738e1a1af6cc9decad29a89d40266af109d2
Aapo Romu <aapo.romu@eficode.com>
2015-12-25 16:31:56 +0200

    First version, reading two values from arduino eith python

--- Theremin/Theremin.ino
+++ Theremin/Theremin.ino
@@ -5,15 +5,16 @@
 
  circuit:
  * 8-ohm speaker on digital pin 9
- * photoresistor on analog 0 to 5V
- * 4.7K resistor on analog 0 to ground
+ * ultrasonic sensor
+ * couple of resistors
 
  created 21 Jan 2010
  modified 31 May 2012
  by Tom Igoe, with suggestion from Michael Flynn
-
+ Hacked for own purposes 2015 by Aapo Romu
 This example code is in the public domain.
 
+Original example for photoresistor:
  http://www.arduino.cc/en/Tutorial/Tone2
 
  */
@@ -22,7 +23,7 @@ This example code is in the public domain.
 #define TRIGPIN 12
 
 void setup() {
-  // initialize serial communications (for debugging only):
+  // initialize serial communications (for feeding python ui):
   Serial.begin(19200);
   pinMode(ECHOPIN, INPUT);
   pinMode(TRIGPIN, OUTPUT);
@@ -36,37 +37,22 @@ void loop() {
   delayMicroseconds(10);
   digitalWrite(TRIGPIN, LOW);
 
-  float distance = pulseIn(ECHOPIN, HIGH);
-  distance=distance/2+100;
-  
-  //Serial.println(" cm");
-  
-  
-  
-  //int sensorReading = analogRead(A0);
-  // print the sensor reading so you know its range
-  //Serial.println(sensorReading);
-  // map the analog input range (in this case, 400 - 1000 from the photoresistor)
-  // to the output pitch range (120 - 1500Hz)
-  // change the minimum and maximum input numbers below
-  // depending on the range your sensor's giving:
-  //int thisPitch = map(distance, 400, 1000, 120, 1500);
-  //Serial.print("D: ");
-  //Serial.print(distance);
-  //Serial.print(" P: ");
-  //Serial.println(thisPitch);
+  float sensorValue = pulseIn(ECHOPIN, HIGH);
+  float pitch=(sensorValue/2)+100;
+
   // play the pitch:
-  if(distance > 40 && distance < 3000) {
+  if(pitch > 40 && pitch < 3000) {
+    Serial.print("S:");
+    Serial.println(sensorValue, DEC);
+    Serial.print("P:");
+    Serial.println(pitch, DEC);
+
     noTone(9);
-    tone(9, distance, 50);
+    tone(9, pitch, 50);
   } else {
     noTone(9);
   }
   delay(5);        // delay in between reads for stability
 }
-
-
-
-
 
 
---
+++ theremin-ui/ui/arduino.py
@@ -0,0 +1,74 @@
+"""
+Listen to serial and gether the data in buffer
+"""
+
+from threading import Thread
+import time
+import serial
+
+sensor = []
+pitch = []
+running = False
+def receiving(ser):
+    global sensor
+    global pitch
+    global running
+    previousSensor = -1
+    previousPitch = -1
+    while running:
+        raw = ser.readline()
+        if raw:
+            data = raw.decode().strip()
+            value = data.split(":")
+            if value[0] == "S":
+                previousSensor = value[1]
+                sensor.append(previousSensor)
+                pitch.append(previousPitch)
+            else:
+                previousPitch = value[1]
+                sensor.append(previousSensor)
+                pitch.append(previousPitch)
+
+class SerialData(object):
+    def __init__(self, init=50):
+        try:
+            self.ser = ser = serial.Serial(
+                port='/dev/tty.usbmodem1421',
+                baudrate=19200,
+                bytesize=serial.EIGHTBITS,
+                parity=serial.PARITY_NONE,
+                stopbits=serial.STOPBITS_ONE,
+                timeout=0.1,
+                xonxoff=0,
+                rtscts=0,
+                interCharTimeout=None
+            )
+        except serial.serialutil.SerialException:
+            #no serial connection
+            self.ser = None
+        else:
+            Thread(target=receiving, args=(self.ser,)).start()
+
+    def next(self):
+        if not self.ser:
+            return -1
+        if sensor.__len__() > 0:
+            return (sensor.pop(0), pitch.pop(0))
+        #if pitch.__len__() > 0:
+        #    sValue = sensor.pop(0)
+        return None
+
+    def __del__(self):
+        if self.ser:
+            self.ser.close()
+
+if __name__=='__main__':
+    running = True
+    s = SerialData()
+    for i in range(500):
+        time.sleep(.010)
+        value = s.next()
+        if(value):
+            print('Buffer size: ' + str(sensor.__len__()) + ' sensor: ' + str(float(value[0])) + " pitch: " + str(float(value[1])))
+
+    running = False
